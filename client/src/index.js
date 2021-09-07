9dc1423d30e080814ae875273b5b164462f0fc54
Karin Ohman <karin.ohman@devetry.com>
2021-09-07 10:36:40 -0600

    Update for deploy

--- client/src/index.js
+++ client/src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
+  uri: 'https://server-castronauts.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
